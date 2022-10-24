aa56c75936b0cb221f6408756d949100bcf58820
Ryan S <ryansateles@gmail.com>
2022-10-24 18:26:10 -0300

    Home screen started

---
+++ src/Pages/Home/Home.tsx
@@ -0,0 +1,14 @@
+import NavBar from "@/components/NavBar/NavBar";
+import Footer from "@/components/Footer/Footer";
+import React from "react";
+
+const Home = () => {
+    return (
+        <div>
+            <NavBar />
+            <Footer />
+        </div>
+    );
+};
+
+export default Home;
