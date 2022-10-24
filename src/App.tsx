3ebfbac800b7df4b705dd109ce6f1016655da42d
Ryan S <ryansateles@gmail.com>
2022-10-24 18:24:06 -0300

    import react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from "react";
-import NavBar from "./components/NavBar/NavBar";
+import { Route, Routes } from "react-router-dom";
+import Home from "@/Pages/Home/Home";
+import Login from "@/Pages/Login/Login";
+//import Footer from "@/components/Footer/Footer";
 
 function App() {
     return (
         <div className="App">
-            <NavBar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+            </Routes>
+            {/* <Footer /> */}
         </div>
     );
 }
