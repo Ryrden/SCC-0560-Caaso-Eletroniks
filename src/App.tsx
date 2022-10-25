f89438c4b69ddf909f8339aa75daa8aeb233416b
joelcoelho_ <joelfeco4@gmail.com>
2022-10-25 01:08:48 -0300

    chore: fixed eslint issues

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,17 @@ import { Route, Routes } from "react-router-dom";
 import Home from "@/Pages/Home/Home";
 import Login from "@/Pages/Login/Login";
 import Footer from "@/components/Footer/Footer";
+import NavBar from "@/components/NavBar/NavBar";
 
 function App() {
     return (
         <div className="App">
             <Routes>
+                <NavBar />
                 <Route path="/" element={<Home />} />
+                <Footer />
                 <Route path="/login" element={<Login />} />
             </Routes>
-            {/* <Footer /> */}
         </div>
     );
 }
