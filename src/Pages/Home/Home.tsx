64b508a983480bf06030ce6f2bbdbb593d68c777
joelcoelho_ <joelfeco4@gmail.com>
2022-10-25 02:03:07 -0300

    feat: mvp login, product card and finished theme

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,11 +1,22 @@
 import NavBar from "@/components/NavBar/NavBar";
 import Footer from "@/components/Footer/Footer";
 import React from "react";
+import ProductCard from "@/components/ProductCard/ProductCard";
 
 const Home = () => {
     return (
         <div>
             <NavBar />
+            <ProductCard 
+                productId={"1"} 
+                productImgSource={""} title={"sadasdas"} 
+                pricing={{
+                    fullPrice: "R$ 1.000,00",
+                    hasPromotion: true,
+                    promotionalPrice: "R$ 1.000,00",
+                    promotionWithPaymentMethod: "via pix"
+                }} 
+                freeShipping={true} />
             <Footer />
         </div>
     );
