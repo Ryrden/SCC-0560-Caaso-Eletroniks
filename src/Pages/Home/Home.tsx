3ba04d1af60c3ddb4c569dd7ffc829888721a25d
joelcoelho_ <joelfeco4@gmail.com>
2022-10-28 19:16:42 -0300

    feat: started home layout

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,22 +1,72 @@
+import React, { useEffect, useState } from "react";
 import NavBar from "@/components/NavBar/NavBar";
 import Footer from "@/components/Footer/Footer";
-import React from "react";
-import ProductCard from "@/components/ProductCard/ProductCard";
+import ProductCard, { ProductCardTypes } from "@/components/ProductCard/ProductCard";
+import { Box, colors, CssBaseline, Grid, Typography } from "@mui/material";
+import CButton from "@/components/Button/Button";
+import { productsList } from "./products";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 
 const Home = () => {
+
+    const [products, setProducts] = useState<ProductCardTypes[]>([]);
+
+    useEffect(() => {
+        setProducts(productsList);
+    }, []);
+
+    function onSeeMore() {
+        setProducts([...products, ...productsList]);
+    }
+
     return (
         <div>
+            <CssBaseline />
             <NavBar />
-            <ProductCard 
-                productId={"1"} 
-                productImgSource={""} title={"sadasdas"} 
-                pricing={{
-                    fullPrice: "R$ 1.000,00",
-                    hasPromotion: true,
-                    promotionalPrice: "R$ 1.000,00",
-                    promotionWithPaymentMethod: "via pix"
-                }} 
-                freeShipping={true} />
+            
+            <Box maxWidth={"1100px"} marginX={"auto"} marginTop={"46px"} marginBottom={"90px"}>
+                <Box borderBottom={5} borderColor={"primary.main"} width={"19.5%"} paddingBottom={"8px"}>
+                    <Typography variant="h4" color={colors.grey.A100} fontWeight={"bold"}>Em promoção</Typography>
+                </Box>
+
+                <Grid container gap={2} width={"100%"} justifyContent={"space-between"} marginTop={"48px"} marginBottom={"40px"}>
+                    {products.map((product, index) => {
+                        return <ProductCard
+                            key={index}
+                            {...product}
+                        />;
+                    })}
+                </Grid>
+
+                <Grid container justifyContent="flex-end">
+                    <CButton onClick={onSeeMore} endIcon={<ArrowForwardIcon />} fullWidth={false}>
+                        Ver mais
+                    </CButton>
+                </Grid>
+
+            </Box>
+
+            <Box maxWidth={"1100px"} marginX={"auto"} marginTop={"46px"} marginBottom={"90px"}>
+                <Box borderBottom={5} borderColor={"primary.main"} width={"20.5%"} paddingBottom={"8px"}>
+                    <Typography variant="h4" color={colors.grey.A100} fontWeight={"bold"}>Mais vendidos</Typography>
+                </Box>
+
+                <Grid container gap={2} width={"100%"} justifyContent={"space-between"} marginTop={"48px"} marginBottom={"40px"}>
+                    {products.map((product, index) => {
+                        return <ProductCard
+                            key={index}
+                            {...product}
+                        />;
+                    })}
+                </Grid>
+
+                <Grid container justifyContent="flex-end">
+                    <CButton onClick={onSeeMore} endIcon={<ArrowForwardIcon />} fullWidth={false}>
+                        Ver mais
+                    </CButton>
+                </Grid>
+            </Box>
+
             <Footer />
         </div>
     );
---
+++ src/Pages/Home/products.ts
@@ -0,0 +1,50 @@
+export const productsList = [
+    {
+        productId: "1",
+        productImgSource: "https://picsum.photos/seed/picsum/200/300", 
+        title: "PC Gamer Concórdia AMD Ryzen",
+        pricing: {
+            fullPrice: "R$ 1.000,00",
+            hasPromotion: true,
+            promotionalPrice: "R$ 1.000,00",
+            promotionWithPaymentMethod: "via pix"
+        },
+        freeShipping: true,
+    },
+    {
+        productId: "1",
+        productImgSource: "https://picsum.photos/seed/picsum/200/300", 
+        title: "PC Gamer Concórdia AMD Ryzen",
+        pricing: {
+            fullPrice: "R$ 1.000,00",
+            hasPromotion: true,
+            promotionalPrice: "R$ 1.000,00",
+            promotionWithPaymentMethod: "via pix"
+        },
+        freeShipping: true,
+    },
+    {
+        productId: "1",
+        productImgSource: "https://picsum.photos/seed/picsum/200/300", 
+        title: "PC Gamer Concórdia AMD Ryzen",
+        pricing: {
+            fullPrice: "R$ 1.000,00",
+            hasPromotion: true,
+            promotionalPrice: "R$ 1.000,00",
+            promotionWithPaymentMethod: "via pix"
+        },
+        freeShipping: true,
+    },
+    {
+        productId: "1",
+        productImgSource: "https://picsum.photos/seed/picsum/200/300", 
+        title: "PC Gamer Concórdia AMD Ryzen",
+        pricing: {
+            fullPrice: "R$ 1.000,00",
+            hasPromotion: true,
+            promotionalPrice: "R$ 1.000,00",
+            promotionWithPaymentMethod: "via pix"
+        },
+        freeShipping: true,
+    },
+];
