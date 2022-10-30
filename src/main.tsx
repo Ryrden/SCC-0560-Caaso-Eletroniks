f747361df0d87238ccb4a1058d9ea5de226f48eb
Ryan Souza <76923948+Ryrden@users.noreply.github.com>
2022-10-30 18:44:18 -0300

    deploying site (#8)

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
         <React.StrictMode>
             <App />
         </React.StrictMode>
