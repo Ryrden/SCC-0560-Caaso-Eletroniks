58b288f593c066056c84b362ce7e6f922b3effdd
Ryan S <ryansateles@gmail.com>
2022-11-02 00:46:28 -0300

    basename to BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,9 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-    <BrowserRouter>
+    <BrowserRouter basename="/SCC-0560-Caaso-Eletroniks">
         <React.StrictMode>
-            <App/>
+            <App />
         </React.StrictMode>
     </BrowserRouter>
 );
