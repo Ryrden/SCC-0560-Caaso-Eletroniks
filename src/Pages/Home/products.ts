f774d9256e9d5b62480588617033fa84d7c4aedc
mrlFranchi <48134459+mrlFranchi@users.noreply.github.com>
2022-11-30 13:32:54 -0300

    Add more products and images TODO: Product Description

--- src/Pages/Home/products.ts
+++ src/Pages/Home/products.ts
@@ -1,4 +1,12 @@
-import imagemPc from "@/assets/imgs/pcExemplo.svg";
+import imagemPc from "@/assets/imgs/Computers/pcExemplo.svg";
+import PC1 from "@/assets/imgs/Computers/PC1.jpg";
+import PC2 from "@/assets/imgs/Computers/PC2.jpg";
+import PC3 from "@/assets/imgs/Computers/PC3.jpg";
+import PC4 from "@/assets/imgs/Computers/PC4.jpg";
+import PC5 from "@/assets/imgs/Computers/PC5.jpg";
+import PC6 from "@/assets/imgs/Computers/PC6.jpg";
+import PC7 from "@/assets/imgs/Computers/PC7.jpg";
+
 import { ProductModel } from "@/models/ProductModel";
 //Teste
 export const productsList: ProductModel[] = [
@@ -24,7 +32,7 @@ export const productsList: ProductModel[] = [
     },
     {
         id: 2,
-        imgSource: imagemPc,
+        imgSource: PC1,
         title: "PC Gamer Concórdia AMD Ryzen",
         freeShipping: true,
         pricing: {
@@ -44,7 +52,7 @@ export const productsList: ProductModel[] = [
     },
     {
         id: 3,
-        imgSource: imagemPc,
+        imgSource: PC2,
         title: "PC Gamer Concórdia AMD Ryzen",
         freeShipping: true,
         pricing: {
@@ -64,7 +72,88 @@ export const productsList: ProductModel[] = [
     },
     {
         id: 4,
-        imgSource: imagemPc,
+        imgSource: PC3,
+        title: "PC Gamer Concórdia AMD Ryzen",
+        freeShipping: true,
+        pricing: {
+            fullPrice: "R$ 1.000,00",
+            hasPromotion: true,
+            promotionalPrice: "R$ 1.000,00",
+            promotionWithPaymentMethod: "via pix"
+        },
+        description: "PC Gamer Concórdia AMD Ryzen 5 3400G 8GB 1TB Radeon RX Vega 11 Windows 10",
+        specifications: {
+            processor: "AMD Ryzen 5 3400G",
+            ramMemory: "8GB",
+            storage: "1TB",
+            videoCard: "Radeon RX Vega 11",
+            operatingSystem: "Windows 10"
+        }
+    },
+    {
+        id: 5,
+        imgSource: PC4,
+        title: "PC Gamer Concórdia AMD Ryzen",
+        freeShipping: true,
+        pricing: {
+            fullPrice: "R$ 1.000,00",
+            hasPromotion: true,
+            promotionalPrice: "R$ 1.000,00",
+            promotionWithPaymentMethod: "via pix"
+        },
+        description: "PC Gamer Concórdia AMD Ryzen 5 3400G 8GB 1TB Radeon RX Vega 11 Windows 10",
+        specifications: {
+            processor: "AMD Ryzen 5 3400G",
+            ramMemory: "8GB",
+            storage: "1TB",
+            videoCard: "Radeon RX Vega 11",
+            operatingSystem: "Windows 10"
+        }
+    },
+
+    {
+        id: 6,
+        imgSource: PC5,
+        title: "PC Gamer Concórdia AMD Ryzen",
+        freeShipping: true,
+        pricing: {
+            fullPrice: "R$ 1.000,00",
+            hasPromotion: true,
+            promotionalPrice: "R$ 1.000,00",
+            promotionWithPaymentMethod: "via pix"
+        },
+        description: "PC Gamer Concórdia AMD Ryzen 5 3400G 8GB 1TB Radeon RX Vega 11 Windows 10",
+        specifications: {
+            processor: "AMD Ryzen 5 3400G",
+            ramMemory: "8GB",
+            storage: "1TB",
+            videoCard: "Radeon RX Vega 11",
+            operatingSystem: "Windows 10"
+        }
+    },
+    {
+        id: 7,
+        imgSource: PC6,
+        title: "PC Gamer Concórdia AMD Ryzen",
+        freeShipping: true,
+        pricing: {
+            fullPrice: "R$ 1.000,00",
+            hasPromotion: true,
+            promotionalPrice: "R$ 1.000,00",
+            promotionWithPaymentMethod: "via pix"
+        },
+        description: "PC Gamer Concórdia AMD Ryzen 5 3400G 8GB 1TB Radeon RX Vega 11 Windows 10",
+        specifications: {
+            processor: "AMD Ryzen 5 3400G",
+            ramMemory: "8GB",
+            storage: "1TB",
+            videoCard: "Radeon RX Vega 11",
+            operatingSystem: "Windows 10"
+        }
+    },
+    {
+        id: 8,
+        imgSource: PC7,
         title: "PC Gamer Concórdia AMD Ryzen",
         freeShipping: true,
         pricing: {
