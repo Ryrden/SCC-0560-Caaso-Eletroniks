0ce7b349c6beb66cb721cfd9beee5523989a00f8
Ryan Souza <76923948+Ryrden@users.noreply.github.com>
2022-12-02 22:04:48 -0300

    Feature/checkout screen (#16)
    * Página de Pagamento
    
    * Página de Pagamento
    
    * renomeei Pagamento para Checkout
    
    * removi imports desnecessarios e apaguei pasta com nome do projeto
    
    * tela de pagamento completa e de sucesso parcialmente completa
    
    * Tela de sucesso pronta e instalei o x-data-picker no package
    
    Co-authored-by: Moniely Barboza <91749118+moniely-barboza@users.noreply.github.com>
    Co-authored-by: Moniely Barboza <moniely-barboza@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import { Route, Routes } from "react-router-dom";
 import Home from "@/Pages/Home/Home";
 import Login from "@/Pages/LoginSignIn/Login/Login";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Checkout from "@/Pages/Checkout/Checkout";
 import Cart from "./Pages/Cart/Cart";
-import SignIn from "@/Pages/LoginSignIn/SignIn/SignIn";
+import Success from "@/Pages/Success/Success";
+import SignIn from "@/Pages/LoginSignIn/Login/Login";
 
 const theme = createTheme({
     palette: {
@@ -15,13 +17,13 @@ const theme = createTheme({
             main: "#270039", //Roxo Chique
         },
         success: {
-            main: "#18F27E", //Verde Claro
+            main: "#048B43", //Verde Claro
         },
         info: {
-            main: "#F7F7F7",
+            main: "#F7F7F7", // Branco
         },
         background: {
-            default: "#131313",
+            default: "#131313", // Preto
         },
         divider: "#565656"
     },
@@ -34,6 +36,9 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/login" element={<Login />} />
+                    <Route path="/checkout" element={<Checkout />} />
+                    <Route path="/carrinho" element={<Cart />} />
+                    <Route path="/success" element={<Success />} />
                     <Route path="/signin" element={<SignIn />} />
                     <Route path="/checkout" element={<Cart />} />
                 </Routes>
