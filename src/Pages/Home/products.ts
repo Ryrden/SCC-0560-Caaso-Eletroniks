7120befb8db72f0110cc813616345f1572ccecf4
mrlFranchi <48134459+mrlFranchi@users.noreply.github.com>
2022-12-04 17:05:11 -0300

    Updated PC names and descriptions

--- src/Pages/Home/products.ts
+++ src/Pages/Home/products.ts
@@ -76,28 +76,28 @@ export const productsList: ProductModel[] = [
     {
         id: 4,
         imgSource: PC3,
-        title: "PC Gamer Concórdia AMD Ryzen",
+        title: "Setup Completo CasaOffice Starter Intel i3",
         freeShipping: true,
         pricing: {
-            fullPrice: "R$ 1.000,00",
+            fullPrice: "R$ 3.500,00",
             hasPromotion: true,
-            promotionalPrice: "R$ 1.000,00",
+            promotionalPrice: "R$ 3.250,00",
             promotionWithPaymentMethod: "via pix"
         },
-        description: "PC Gamer Concórdia AMD Ryzen 5 3400G 8GB 1TB Radeon RX Vega 11 Windows 10",
+        description: "Setup Completo CasaOffice Starter Intel i3 10100 8GB 1TB, inclui monitor Samsung 36 Polegadas, mouse e teclado multilaser.",
         alt: "Setup com Monitor, teclado, mouse e desktop preto, com a tela inicial do Windows.",
         specifications: {
-            processor: "AMD Ryzen 5 3400G",
+            processor: "Intel i3 10100",
             ramMemory: "8GB",
             storage: "1TB",
-            videoCard: "Radeon RX Vega 11",
+            videoCard: "Não Possui",
             operatingSystem: "Windows 10"
         }
     },
     {
         id: 5,
         imgSource: PC2,
-        title: "PC Gamer Concórdia AMD Ryzen",
+        title: "PC Gamer Colibri Intel I3",
         freeShipping: true,
         pricing: {
             fullPrice: "R$ 1.000,00",
@@ -105,13 +105,13 @@ export const productsList: ProductModel[] = [
             promotionalPrice: "R$ 1.000,00",
             promotionWithPaymentMethod: "via pix"
         },
-        description: "PC Gamer Concórdia AMD Ryzen 5 3400G 8GB 1TB Radeon RX Vega 11 Windows 10",
+        description: "PC Gamer Colibri Intel I3 10100 NVIDIA GeForce GTX 1660 8GB SSD 240GB Windows 10",
         alt: "Desktop preto, com a lateral aberta, onde é possivel ver os componentes, com duas ventoinhas, iluminadas com LED azul e vermelho.",
         specifications: {
-            processor: "AMD Ryzen 5 3400G",
+            processor: "Intel I3 10100F",
             ramMemory: "8GB",
-            storage: "1TB",
-            videoCard: "Radeon RX Vega 11",
+            storage: "SSD240GB",
+            videoCard: "NVIDIA GeForce GTX 1660",
             operatingSystem: "Windows 10"
         }
     },
@@ -119,63 +119,63 @@ export const productsList: ProductModel[] = [
     {
         id: 6,
         imgSource: PC5,
-        title: "PC Gamer Concórdia AMD Ryzen",
+        title: "PC Gamer Nirvana AMD Ryzen",
         freeShipping: true,
         pricing: {
-            fullPrice: "R$ 1.000,00",
+            fullPrice: "R$ 3.100,00",
             hasPromotion: true,
-            promotionalPrice: "R$ 1.000,00",
+            promotionalPrice: "R$ 3.000,00",
             promotionWithPaymentMethod: "via pix"
         },
-        description: "PC Gamer Concórdia AMD Ryzen 5 3400G 8GB 1TB Radeon RX Vega 11 Windows 10",
+        description: "PC Gamer Nirvana AMD Ryzen 7 5700X NVIDIA GeForce RTX 3060 16GB SSD480GB",
         alt: "Desktop preto, com a lateral aberta, onde se vê os componentes, com iluminação LED verde.",
         specifications: {
-            processor: "AMD Ryzen 5 3400G",
-            ramMemory: "8GB",
-            storage: "1TB",
-            videoCard: "Radeon RX Vega 11",
+            processor: "AMD Ryzen 7 5700X",
+            ramMemory: "16GB",
+            storage: "SSD480GB",
+            videoCard: "NVIDIA GeForce RTX 3060",
             operatingSystem: "Windows 10"
         }
     },
     {
         id: 7,
         imgSource: PC6,
-        title: "PC Gamer Concórdia AMD Ryzen",
+        title: "PC Gamer Tian AMD Ryzen",
         freeShipping: true,
         pricing: {
-            fullPrice: "R$ 1.000,00",
+            fullPrice: "R$ 2.700,00",
             hasPromotion: true,
-            promotionalPrice: "R$ 1.000,00",
+            promotionalPrice: "R$ 2.500,00",
             promotionWithPaymentMethod: "via pix"
         },
-        description: "PC Gamer Concórdia AMD Ryzen 5 3400G 8GB 1TB Radeon RX Vega 11 Windows 10",
+        description: "PC Gamer Tian AMD Ryzen 5 5600G 8GB SSD 240GB",
         alt: "Computador Desktop preto, com a lateral em vidro, sendo possivel ver os componentes, foco na placa de vídeo.",
         specifications: {
-            processor: "AMD Ryzen 5 3400G",
+            processor: "AMD Ryzen 5 5600G",
             ramMemory: "8GB",
-            storage: "1TB",
-            videoCard: "Radeon RX Vega 11",
+            storage: "SSD240GB",
+            videoCard: "Não Possui",
             operatingSystem: "Windows 10"
         }
     },
     {
         id: 8,
         imgSource: PC7,
-        title: "PC Gamer Concórdia AMD Ryzen",
+        title: "Workstation Go 2A Xeonw-2255",
         freeShipping: true,
         pricing: {
-            fullPrice: "R$ 1.000,00",
+            fullPrice: "R$ 35.272,00",
             hasPromotion: true,
-            promotionalPrice: "R$ 1.000,00",
+            promotionalPrice: "R$ 33.378,00",
             promotionWithPaymentMethod: "via pix"
         },
-        description: "PC Gamer Concórdia AMD Ryzen 5 3400G 8GB 1TB Radeon RX Vega 11 Windows 10",
+        description: Workstation Go 2A Xeonw-2255 NVIDIA RTX 4000 64GB SSD 1TB ",
         alt: "Computador Desktop preto, com a lateral em vidro, sendo possível ver os componentes, iluminados por LED Branco.",
         specifications: {
-            processor: "AMD Ryzen 5 3400G",
-            ramMemory: "8GB",
-            storage: "1TB",
-            videoCard: "Radeon RX Vega 11",
+            processor: "Xeonw-2255",
+            ramMemory: "64GB",
+            storage: "SSD1TB",
+            videoCard: "NVIDIA RTX 4000",
             operatingSystem: "Windows 10"
         }
     },
