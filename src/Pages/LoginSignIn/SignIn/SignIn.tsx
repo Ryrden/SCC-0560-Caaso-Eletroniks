a821f8db4f4b1e399a63f0f55b1943f34f8261aa
Moniely Barboza <91749118+moniely-barboza@users.noreply.github.com>
2022-12-09 19:23:49 -0300

    Mascaras padronizadas da forma do ryan

--- src/Pages/LoginSignIn/SignIn/SignIn.tsx
+++ src/Pages/LoginSignIn/SignIn/SignIn.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@/components/Input/Input";
+import InputMask from "react-input-mask";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
 import Grid from "@mui/material/Grid";
@@ -42,15 +43,6 @@ const SignIn = () => {
         setCurrency(event.target.value);
     };
 
-    const [cpfValue, setCpfValue] = React.useState("");
-    const [cpfValid, setCpfValid] = React.useState(true);
-    const handleCpfValidation = (e: any) => {
-        const regex = new RegExp("[0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2}");
-        setCpfValid(regex.test(e.target.value));
-        setCpfValue(e.target.value);
-    };
-
-
     const [telValue, setTelValue] = React.useState("");
     const [telValid, setTelValid] = React.useState(true);
     const handleTelValidation = (e: any) => {
@@ -68,6 +60,10 @@ const SignIn = () => {
         setCepValue(e.target.value);
     };
 
+    const [birth, setBirth] = React.useState("");
+    const [cpf, setCpf] = React.useState("");
+    const [tel, setTel] = React.useState("");
+    const [cep, setCep] = React.useState("");
 
     return (
         <>
@@ -98,40 +94,51 @@ const SignIn = () => {
                                     />
                                 </Grid>
                                 <Grid item xs={12} sm={6}>
-                                    <TextField
-                                        label="Data de Nascimento"
-                                        name={"date"}
-                                        required
-                                        type={"date"}
-                                        focused
-                                    />
+                                    <InputMask
+                                        mask="99/99"
+                                        value={birth}
+                                        placeholder="00/00"
+                                        onChange={(e) => setBirth(e.target.value)}
+                                    >
+                                        {() =>
+                                            <TextField
+                                                required
+                                                label="Data de Nascimento"
+                                                name={"data-nasc"}
+                                                type={"text"}
+                                            />}
+                                    </InputMask>
                                 </Grid>
                             </Grid>
 
                             <Grid container spacing={3} marginBottom={"20px"}>
                                 <Grid item xs={12} sm={6}>
-                                    <TextField
-                                        required
-                                        label="CPF"
-                                        name={"cpf"}
-                                        inputProps={{ maxlength: "14" }}
-                                        value={cpfValue}
-                                        error={!cpfValid}
-                                        placeholder={"000.000.000-00"}
-                                        onChange={(e) => handleCpfValidation(e)}
-                                    />
+                                    <InputMask
+                                        mask="999\.999\.999\-99"
+                                        value={cpf}
+                                        placeholder="000.000.000-00"
+                                        onChange={(e) => setCpf(e.target.value)}
+                                    >
+                                        {() => <TextField
+                                            required
+                                            label="CPF"
+                                            name={"cpf"}
+                                        />}
+                                    </InputMask>
                                 </Grid>
                                 <Grid item xs={12} sm={6}>
-                                    <TextField
-                                        label="Telefone Celular"
-                                        name={"telefone"}
-                                        inputProps={{ maxlength: "11" }}
-                                        value={telValue}
-                                        error={!telValid}
-                                        placeholder={"Apenas números"}
-                                        onChange={(e) => handleTelValidation(e)}
-                                        required
-                                    />
+                                    <InputMask
+                                        mask="\(99\) 99999\-9999"
+                                        value={tel}
+                                        placeholder="(99) 99999-9999"
+                                        onChange={(e) => setTel(e.target.value)}
+                                    >
+                                        {() => <TextField
+                                            required
+                                            label="Telefone"
+                                            name={"telefone"}
+                                        />}
+                                    </InputMask>
                                 </Grid>
                             </Grid>
 
@@ -174,16 +181,18 @@ const SignIn = () => {
 
                             <Grid container spacing={3} marginBottom={"20px"}>
                                 <Grid item xs={12} md={12}>
-                                    <TextField
-                                        label="CEP"
-                                        name={"cep"}
-                                        inputProps={{ maxlength: "9" }}
-                                        value={cepValue}
-                                        error={!cepValid}
-                                        placeholder={"00000-000"}
-                                        onChange={(e) => handleCepValidation(e)}
-                                        required
-                                    />
+                                    <InputMask
+                                        mask="99999\-99"
+                                        value={cep}
+                                        placeholder="00000-00"
+                                        onChange={(e) => setCep(e.target.value)}
+                                    >
+                                        {() => <TextField
+                                            required
+                                            label="CEP"
+                                            name={"cep"}
+                                        />}
+                                    </InputMask>
                                 </Grid>
                             </Grid>
 
