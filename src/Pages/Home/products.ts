cd3817a149030065b32bb8b150845f6078bfb0aa
mrlFranchi <48134459+mrlFranchi@users.noreply.github.com>
2022-12-09 14:43:15 -0300

    Product descriptions & README (#23)
    * Updatetd Products descriptions
    
    * Update README.md
    
    * Update README.md

--- src/Pages/Home/products.ts
+++ src/Pages/Home/products.ts
@@ -21,7 +21,7 @@ export const productsList: ProductModel[] = [
             promotionalPrice: "R$ 3.750,00",
             promotionWithPaymentMethod: "via pix"
         },
-        description: "PC Gamer Concórdia AMD Ryzen 5 3400G 8GB 1TB Radeon RX Vega 11 Windows 10",
+        description: "O PC Gamer Concórdia consta com o poderoso AMD Ryzen 5 3400G, com seus 8GB de memoria RAM e sua placa de video Radeon RX Vega 11 você podera arrasar nos melhores jogos da ultima gerção, Com seu HD de 1TB você tem espaço suficiente para instalar todos os jogos que quiser.",
         alt: "Computador Desktop, cor grafite, com a lateral aberta, podendo ver os componentes iluminados por LED branco azulado.",
         specifications: {
             processor: "AMD Ryzen 5 3400G",
@@ -42,12 +42,12 @@ export const productsList: ProductModel[] = [
             promotionalPrice: "R$ 2.500,00",
             promotionWithPaymentMethod: "via pix"
         },
-        description: "Computador CasaOffice TECNOART Intel i3 10100 8GB SSD 240GB",
+        description: "O Computador CasaOffice TECNOART é perfeito para sua familia, possui o poderoso processador Intel i3 10100 que, junto com seus, 8GB de memória garantem uuma navegação suave e responsiva. Com armazenamento suficiente para salvar todos os seus arquivos graças ao HD de 1TB",
         alt: "Computador desktop branco com lateral aberta, dentro se vê os componentes do PC iluminados por LED RGB colorido",
         specifications: {
             processor: "Intel i3 10100",
             ramMemory: "8GB",
-            storage: "SSD240GB",
+            storage: "1TB",
             videoCard: "Não Possui",
             operatingSystem: "Windows 10"
         }
@@ -58,12 +58,12 @@ export const productsList: ProductModel[] = [
         title: "PC Gamer Falcon Intel i5 8GB de RAM",
         freeShipping: true,
         pricing: {
-            fullPrice: "R$ 4.500,00",
+            fullPrice: "R$ 4.200,00",
             hasPromotion: true,
             promotionalPrice: "R$ 4.000,00",
             promotionWithPaymentMethod: "via pix"
         },
-        description: "PC Gamer Inferno Intel i5 10400F NVIDIA GeForce RTX 3060 8GB SSD 240GB Water Colling",
+        description: "Com o incrivel PC Gamer Inferno, você será capaz de rodar os maiores jogos da atualidade graças a sua placa de vídeo NVIDIA GeForce RTX 3060, com fantastico desempenho graças ao seu processador Intel i5 10400F com 8 GB de memória. O reesfriamento a água garante que até mesmo seu PC Gamer Inferno fique fresco. Um SSD de 240 GB garante muito espaço com uma velocidade de transferencia de dados extremamente rapida.",
         alt: "Computador desktop preto, com a lateral aberta, dentro se vê os componentes, iluminados por LED vermelhos.",
         specifications: {
             processor: "Intel i5 10400F",
@@ -79,12 +79,12 @@ export const productsList: ProductModel[] = [
         title: "Setup Completo CasaOffice Starter Intel i3",
         freeShipping: true,
         pricing: {
-            fullPrice: "R$ 3.500,00",
+            fullPrice: "R$ 2.500,00",
             hasPromotion: true,
-            promotionalPrice: "R$ 3.250,00",
+            promotionalPrice: "R$ 2.250,00",
             promotionWithPaymentMethod: "via pix"
         },
-        description: "Setup Completo CasaOffice Starter Intel i3 10100 8GB 1TB, inclui monitor Samsung 36 Polegadas, mouse e teclado multilaser.",
+        description: "O Setup Completo CasaOffice Starter é perfeito para quem precisa de uma maquina poderosa, mas tem um baixo orçamento. Seu processador Intel i3 10100 e sus 8GB de memória garantem uma navegação fluida e sem travamentos. Com um HD de 1TB garante bastante espaço para guardar seus documentos, fotos, videos, e jogos, inclui monitor Samsung 36 Polegadas com qualidade de imagem incrivel, mouse e teclado de qualidade pela multilaser.",
         alt: "Setup com Monitor, teclado, mouse e desktop preto, com a tela inicial do Windows.",
         specifications: {
             processor: "Intel i3 10100",
@@ -105,7 +105,7 @@ export const productsList: ProductModel[] = [
             promotionalPrice: "R$ 2.000,00",
             promotionWithPaymentMethod: "via pix"
         },
-        description: "PC Gamer Colibri Intel I3 10100 NVIDIA GeForce GTX 1660 8GB SSD 240GB Windows 10",
+        description: "O PC Gamer Colibri é perfeito para quem quer iniciar no mundo dos games, mas não tem um grande orçamento. Com o processador Intel I3 10100 e 8GB de RAM garantem uma excelente performance nos jogos enquanto a placa de vídeo NVIDIA GeForce GTX 1660 garante uma excelencia em qualidade de imagem. Um SSD 240GB é espaço suficiente para salvar varios jogos, com uma excelente taxa de dados.",
         alt: "Desktop preto, com a lateral aberta, onde é possivel ver os componentes, com duas ventoinhas, iluminadas com LED azul e vermelho.",
         specifications: {
             processor: "Intel I3 10100F",
@@ -127,7 +127,7 @@ export const productsList: ProductModel[] = [
             promotionalPrice: "R$ 3.000,00",
             promotionWithPaymentMethod: "via pix"
         },
-        description: "PC Gamer Nirvana AMD Ryzen 7 5700X NVIDIA GeForce RTX 3060 16GB SSD480GB",
+        description: "O PC Gamer Nirvana leva a performace dos jogos até os céus. Seu preocessador AMD Ryzen 7 5700X é extremamene poderoso, combinado com incriveis 16GB de memória e a poderosa placa GeForce RTX 3060 da NVIDIA. E com um SSD de 480GB permie que você instale qualquer jogo que você quiser.",
         alt: "Desktop preto, com a lateral aberta, onde se vê os componentes, com iluminação LED verde.",
         specifications: {
             processor: "AMD Ryzen 7 5700X",
@@ -148,7 +148,7 @@ export const productsList: ProductModel[] = [
             promotionalPrice: "R$ 2.500,00",
             promotionWithPaymentMethod: "via pix"
         },
-        description: "PC Gamer Tian AMD Ryzen 5 5600G 8GB SSD 240GB",
+        description: "Com o PC Gamer Tian, consta com o poderoso porcessador AMD Ryzen 5 5600G, com 8GB garantem um excelente desempenho nos jogos da atualidade. Um SSD de 240GB é espaço suficiente para instalar tais jogos.",
         alt: "Computador Desktop preto, com a lateral em vidro, sendo possivel ver os componentes, foco na placa de vídeo.",
         specifications: {
             processor: "AMD Ryzen 5 5600G",
@@ -166,10 +166,10 @@ export const productsList: ProductModel[] = [
         pricing: {
             fullPrice: "R$ 35.272,00",
             hasPromotion: true,
-            promotionalPrice: "R$ 33.378,00",
+            promotionalPrice: "R$ 33.369,00",
             promotionWithPaymentMethod: "via pix"
         },
-        description: "Workstation Go 2A Xeonw-2255 NVIDIA RTX 4000 64GB SSD 1TB ",
+        description: "A Workstation Go 2A é uma estaçã de trabalho para os mlehores. Seu processador Xeonw-2255, com placa de video NVIDIA RTX 4000 e absurdos 64GB de memoria e um SSD de 1TB garantem o melhor desempenho para os softwres mais pesados. Edição de imagem, vídeo, simulações e entre muitos outros trabalhos que necessitam de um processamento extremo.",
         alt: "Computador Desktop preto, com a lateral em vidro, sendo possível ver os componentes, iluminados por LED Branco.",
         specifications: {
             processor: "Xeonw-2255",
